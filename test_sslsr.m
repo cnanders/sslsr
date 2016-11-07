5fd3786d5cc1ada9645aeb6792b6cbd94fe88905
Chris Anderson <cnanders@gmail.com>
2016-11-07 12:56:19 -0800

    don't now

--- test_sslsr.m
+++ test_sslsr.m
@@ -1,9 +1,8 @@
 
-
 [cPath, cName, cExt] = fileparts(mfilename('fullpath'));
 
 % Add mic
-addpath(genpath(fullfile(cPath, '..', 'lib', 'mic')));
+addpath(genpath(fullfile(cPath, 'lib', 'mic')));
 
 purge;
 
